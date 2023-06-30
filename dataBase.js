bdbe84a106d3c8731754557156c0a3815e97bb29
Khoa Le Rikkei <116610704+khoalyRikkei@users.noreply.github.com>
2023-06-30 07:58:33 +0700

    session 20 - ES6

---
+++ dataBase.js
@@ -0,0 +1,55 @@
+// Các 1: export theo tên
+
+export const students = [
+  {
+    id: "SV1",
+    name: "Nguyên Văn A",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+  {
+    id: "SV2",
+    name: "Nguyên Văn B",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+  {
+    id: "SV3",
+    name: "Nguyên Văn C",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+  {
+    id: "SV4",
+    name: "Nguyên Văn D",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+  {
+    id: "SV5",
+    name: "Nguyên Văn E",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+];
+
+const data = [
+  {
+    id: "SV5",
+    name: "Nguyên Văn E",
+    address: "ĐN",
+    gender: true,
+    class: "RA_01",
+  },
+];
+
+function App() {
+  return `<h1>Hello world</h1>`;
+}
+
+export default App;
